726370cb15e74a67ba17034330c3e94ed93824a0
HarikaGutha <61112886+HarikaGutha@users.noreply.github.com>
2021-02-19 17:48:50 +0000

    Add files via upload

---
+++ teamviewer/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'teamviewer'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'TeamViewer_Setup'
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'exe'
+  file         = $fileLocation
+
+  silentArgs    = "/S"
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
